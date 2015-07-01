176951a3d9dc30950d7caa92ff8024d90814a9f5
Vitalik Dydko <vitalikdudko127@gmail.com>
2015-07-01 23:37:40 +0300

    HelpDesk admin part

---
+++ HelpDesk/HelpDesk/Models/HelpdeskContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelpDesk.Models
+{
+    public class HelpdeskContext:DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Lifecycle> Lifecycles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Activ> Activs { get; set; }
+    }
+}
